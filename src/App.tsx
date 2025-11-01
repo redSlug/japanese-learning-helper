8f29fcf2359674dddb7fa2c16b877b5642594734
bradley <11279144+redSlug@users.noreply.github.com>
2025-10-31 23:26:36 -0400

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -3,57 +3,13 @@ import "./App.css";
 import { FlashCard } from "./types/FlashCard.ts";
 import { DeckSelector } from "./components/DeckSelector.tsx";
 import Card from "./components/Card.tsx";
+import { availableDecks } from "./data/decks.ts";
 
 function FlashCardApp() {
   const [currentIndex, setCurrentIndex] = useState(0);
   const [isFlipped, setIsFlipped] = useState(false);
   const [cards, setCards] = useState<Array<FlashCard>>([]);
   const [selectedDeck, setSelectedDeck] = useState<string>("activities.json");
-  const availableDecks = [
-    {
-      label_english: "This App",
-      label_hiragana: "このあぷり",
-      file: "app.json",
-    },
-    {
-      label_english: "Activities",
-      label_hiragana: "かつどう",
-      file: "activities.json",
-    },
-    {
-      label_english: "Adjectives",
-      label_hiragana: "けいようし",
-      file: "adjectives.json",
-    },
-    {
-      label_english: "Classroom",
-      label_hiragana: "きょうしつ",
-      file: "classroom.json",
-    },
-    {
-      label_english: "Temporal",
-      label_hiragana: "じかん",
-      file: "temporal.json",
-    },
-    { label_english: "Food", label_hiragana: "たべもの", file: "food.json" },
-    {
-      label_english: "Position",
-      label_hiragana: "いち",
-      file: "position.json",
-    },
-    { label_english: "Colors", label_hiragana: "いろ", file: "colors.json" },
-    { label_english: "Home", label_hiragana: "いえ", file: "home.json" },
-    {
-      label_english: "Grammar",
-      label_hiragana: "ぶんぽう",
-      file: "grammar.json",
-    },
-    {
-      label_english: "Counting",
-      label_hiragana: "かぞえかた",
-      file: "counting.json",
-    },
-  ];
 
   useEffect(() => {
     const fetchData = async () => {
---
+++ src/data/decks.ts
@@ -0,0 +1,37 @@
+export const availableDecks = [
+  { label_english: "This App", label_hiragana: "このあぷり", file: "app.json" },
+  {
+    label_english: "Activities",
+    label_hiragana: "かつどう",
+    file: "activities.json",
+  },
+  {
+    label_english: "Adjectives",
+    label_hiragana: "けいようし",
+    file: "adjectives.json",
+  },
+  {
+    label_english: "Classroom",
+    label_hiragana: "きょうしつ",
+    file: "classroom.json",
+  },
+  {
+    label_english: "Temporal",
+    label_hiragana: "じかん",
+    file: "temporal.json",
+  },
+  { label_english: "Food", label_hiragana: "たべもの", file: "food.json" },
+  { label_english: "Position", label_hiragana: "いち", file: "position.json" },
+  { label_english: "Colors", label_hiragana: "いろ", file: "colors.json" },
+  { label_english: "Home", label_hiragana: "いえ", file: "home.json" },
+  {
+    label_english: "Grammar",
+    label_hiragana: "ぶんぽう",
+    file: "grammar.json",
+  },
+  {
+    label_english: "Counting",
+    label_hiragana: "かぞえかた",
+    file: "counting.json",
+  },
+];
